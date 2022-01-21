9fab946d050e6f2fda5dc5682a8fc3ea132e95c3
Paulo Alenquer <pauloalenquer@gmail.com>
2022-01-21 16:54:29 -0300

    added onChange function and render button to home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,40 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import axios from "axios";
+import { UiFileInputButton } from "../components/UiFileInputButton";
 
 const Home: NextPage = () => {
+  const onChange = async (formData: FormData) => {
+    const config = {
+      headers: { "content-type": "multipart/form-data" },
+      onUploadProgress: (event: any) => {
+        console.log(
+          `Current progress:`,
+          Math.round((event.loaded * 100) / event.total)
+        );
+      },
+    };
+
+    const response = await axios.post("/api/uploads", formData, config);
+
+    console.log("response", response.data);
+  };
+
   return (
-    <div className="h-screen flex justify-center items-center">
+    <>
       <Head>
         <title>Home page</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <button className="bg-gray-300 rounded-md p-4">Anexar</button>
-    </div>
+      <div className="h-screen flex justify-center items-center">
+        <UiFileInputButton
+          label="Upload"
+          uploadFileName="theFiles"
+          onChange={onChange}
+        />
+      </div>
+    </>
   );
 };
 
